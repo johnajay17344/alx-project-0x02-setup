1a6e43200280ffc8a6c18a3f0d62485989298a43
johnajay17344 <adekunlegold300@gmail.com>
2025-09-26 09:04:26 +0100

    aleshhddf

--- alx-project-0x02/components/layout/Header.tsx
+++ alx-project-0x02/components/layout/Header.tsx
@@ -1,14 +1,23 @@
-import React from "react";
+// components/layout/Header.tsx
 import Link from "next/link";
+import React from "react";
 
-
-const Header = () => {
+export default function Header() {
   return (
-    <nav className="bg-gray-200 p-4 flex justify-around">
-      <Link href="/home" className="mr-4 text-blue-500">Home</Link>
-      <Link href="/about">About</Link>
-    </nav>
+    <header style={{ padding: "1rem", background: "#f4f4f4" }}>
+      <nav>
+        <ul style={{ display: "flex", gap: "1rem", listStyle: "none" }}>
+          <li>
+            <Link href="/home">Home</Link>
+          </li>
+          <li>
+            <Link href="/about">About</Link>
+          </li>
+          <li>
+            <Link href="/posts">Posts</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
   );
 }
-
-export default Header;
